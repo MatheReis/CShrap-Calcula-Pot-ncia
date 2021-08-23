35462b404e8cef30e64713822d6f14ad68ce07c5
Matheus Reis <83931417+MatheReis@users.noreply.github.com>
2021-08-22 22:40:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CShrap_Calcula_Potência
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int N, p, Pote, J;
+
+          Console.Write("Digite o número para ser potencializado: ");
+          N = int.Parse(Console.ReadLine());
+
+          Console.Write("Digite a potência desejada: ");
+          p = int.Parse(Console.ReadLine());
+
+          while ((p < 0) || (N <= 1))
+          {
+              Console.WriteLine("Entrada inválida");
+              Console.WriteLine();
+              Console.Write("Digite o número para ser potencializado: ");
+              N = int.Parse(Console.ReadLine());
+              Console.Write("Digite a potência desejada: ");
+              p = int.Parse(Console.ReadLine());
+          }
+
+          Pote = 1;
+
+          if (p > 0) //Se a potência é maior que zero
+          {
+              for (J = 1; J <= p; J++)  //calcula a potência com loop 
+              {
+                  Pote = Pote * N;
+              }
+
+          }
+
+          Console.WriteLine();
+          Console.WriteLine($"{N} elevado a {p} : {Pote}");
+        } 
+    }
+}
